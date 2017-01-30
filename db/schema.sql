ae76a3b42f2dd5b98a3261dccc42c97bebc6e977
Peter Tountas <petertountas1@gmail.com>
2017-01-29 18:30:47 -0600

    updated schema, needed a timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -6,5 +6,6 @@ CREATE TABLE burgers (
 id INT(6) AUTO_INCREMENT NOT NULL, 
 burger_name VARCHAR(60) NULL, 
 devoured BOOLEAN NOT NULL,
+data TIMESTAMP null,
 PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,9 @@
-INSERT INTO burgers
-	(burger_name,devoured)
+
+
+
+	INSERT INTO burgers
+	(burger_name,devoured,data)
 VALUES
-	('bacon chedder burger',FALSE),
-	('turkey burger',FALSE),
-	('cheese burger',FALSE);
+	('bacon chedder burger',FALSE, CURRENT_TIMESTAMP),
+	('turkey burger',FALSE, CURRENT_TIMESTAMP),
+	('cheese burger',FALSE, CURRENT_TIMESTAMP);
